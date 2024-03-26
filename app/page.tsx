d2a684bc46779019adbcaaf7ca8943bc61f63736
amirhossein <79211332+capamir@users.noreply.github.com>
2024-03-26 23:54:19 +0330

    home page updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 
@@ -5,14 +7,15 @@ export default function Home() {
   return (
     <main className="w-screen h-screen relative">
       <div
-        className="flex w-full h-full items-center bg-cover bg-center"
-        style={{ backgroundImage: "url(/main-bg.webp" }}
+        className="flex items-center w-full h-full bg-cover bg-center"
+        style={{ backgroundImage: "url(/main-bg.webp)" }}
       >
-        <div className="pl-[40px] md:pl-40 pb-[300px] md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
+        <div className="pl-[50px] md:pl-40 pb-[300px] md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
           <h1 className="text-[50px] text-white font-semibold">
-            Make anything possible with{" "}
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-              programming
+            Make anything possible with
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
+              {" "}
+              Programming
             </span>
           </h1>
           <p className="text-gray-200 hidden md:block">
@@ -22,49 +25,50 @@ export default function Home() {
           <div className="flex-col md:flex-row hidden md:flex gap-5">
             <Link
               href="/my-skills"
-              className="rounded-[20px] group relative bg-blue-500 hover:bg-blue-400 px-5 py-4 text-lg text-white max-w-[200px]"
+              className="rounded-[20px] group relative bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
             >
-              Learn more...
+              Learn more
             </Link>
             <Link
               href="/my-projects"
-              className="rounded-[20px] group relative bg-blue-500 hover:bg-blue-400 px-5 py-4 text-lg text-white max-w-[200px]"
+              className="rounded-[20px] group relative bg-trasparent px-5 border border-white py-3 text-lg text-white max-w-[200px]"
             >
-              {/* <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20" /> */}
-              My Projects
+              <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hver:opacity-20" />
+              My projects
             </Link>
             <Link
               href="/contact-me"
-              className="rounded-[20px] group relative bg-blue-500 hover:bg-blue-400 px-5 py-4 text-lg text-white max-w-[200px]"
+              className="rounded-[20px] group relative bg-trasparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
             >
-              {/* <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20" /> */}
-              Contact Me
+              <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hver:opacity-20" />
+              Contact me
             </Link>
           </div>
         </div>
       </div>
 
-      <div className="absolute flex flex-col bottom-10 z-[20] right-5 md:hidden gap-5">
+      <div className="absolute flex bottom-10 z-[20] right-5 flex-col md:hidden gap-5">
         <Link
           href="/my-skills"
-          className="rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-4 text-lg text-white max-w-[200px]"
+          className="rounded-[20px] group bg-blue-500 px-5 py-3 text-lg text-white max-w-[200px]"
         >
-          Learn more...
+          Learn more
         </Link>
+
         <Link
           href="/my-projects"
-          className="rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-4 text-lg text-white max-w-[200px]"
+          className="rounded-[20px] group bg-trasparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
         >
-          My Projects
+          My projects
         </Link>
         <Link
           href="/contact-me"
-          className="rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-4 text-lg text-white max-w-[200px]"
+          className="rounded-[20px] group bg-trasparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
         >
-          Contact Me
+          Contact me
         </Link>
       </div>
-      {/* hourse */}
+
       <div className="absolute bottom-0 right-0 z-[10]">
         <Image
           src="/horse.png"
@@ -73,19 +77,20 @@ export default function Home() {
           width={300}
           className="absolute right-55 top-40"
         />
+
         <Image src="/cliff.webp" alt="cliff" width={480} height={480} />
       </div>
-      {/* trees */}
+
       <div className="absolute bottom-0 z-[5] w-full h-auto">
         <Image
           src="/trees.webp"
           alt="trees"
           width={2000}
           height={2000}
-          className="h-full w-full"
+          className="w-full h-full"
         />
       </div>
-      {/* stars */}
+
       <Image
         src="/stars.png"
         alt="stars"
