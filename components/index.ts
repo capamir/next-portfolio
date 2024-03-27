5a038e54adb84ee3539d0f7b1ed93b7ff915081e
amirhossein <79211332+capamir@users.noreply.github.com>
2024-03-27 15:44:18 +0330

    contact page

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 export { default as Navbar } from "./Navbar";
 export { default as Navigation } from "./Navigation";
 export { default as ProjectCard } from "./ProjectCard";
+export { default as ContactForm } from "./ContactForm";
